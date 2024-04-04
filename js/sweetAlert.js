b923dcbc80066cd850679f37c1cecd53e561c346
BodaCrisyPau <166055584+BodaCrisyPau@users.noreply.github.com>
2024-04-04 15:49:05 -0300

    Update sweetAlert.js

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -2,7 +2,7 @@ Swal.fire({
 title: "¡¡¡Preparad@s para la boda del año!!!",
 confirmButtonText: "Si, estoy preparad@",
 confirmButtonColor: "#ffbba8",
-imageUrl: "img/amigoss.jpg",
+imageUrl: "img/amigoss.png",
 imageWidth: "400px",
 imageHeight: "200px",
 widht: "80%",
